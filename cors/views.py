bcfbfcc78245eaf485b7fa84c0c333976f466932
taniryla <ryland0000@gmail.com>
2022-02-12 05:11:23 -0700

    added render, fonts, js scripts

--- cors/views.py
+++ cors/views.py
@@ -3,26 +3,26 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Home /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'about.html')
 
 # placed here temporarily until order, store, category app are created
 
 
 def store(request):
-    return HttpResponse('<h1>Home /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'store/store.html')
 
 
 def cart(request):
-    return HttpResponse('<h1>About /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'store/cart.html')
 
 
 def category(request):
-    return HttpResponse('<h1>Home /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Category /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 
 def search(request):
-    return HttpResponse('<h1>About /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Search /ᐠ｡‸｡ᐟ\ﾉ</h1>')
