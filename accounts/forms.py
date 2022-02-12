5d76e9938e51ab9ba4ad740bd6057eacb5767f81
taniryla <ryland0000@gmail.com>
2022-02-12 06:46:56 -0700

    signup function in accounts/views.py

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Account, UserProfile
+
+
+class SignupForm(forms.ModelForm):
+    pass
+
+
+class UserForm(forms.ModelForm):
+    pass
+
+
+class UserProfileForm(forms.ModelForm):
+    pass
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Account, UserProfile
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm, UserForm, UserProfileForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -18,7 +21,23 @@ def login(request):
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'accounts/signup.html', context)
 
 
 def logout(request):
