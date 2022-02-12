6d04196544450c9e3e041ccd54696c4c1de5f70b
taniryla <ryland0000@gmail.com>
2022-02-12 02:37:55 -0700

    added images including logo and banners pre-done

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('dasboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Account, UserProfile
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
 
+
 def dashboard(request):
-    pass
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {'userprofile': userprofile}
+    return render(request, 'accounts/dashboard.html', context)
