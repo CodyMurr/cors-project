7bc0a50498ad9a57bb3185891c545765c63dded7
taniryla <ryland0000@gmail.com>
2022-02-12 07:53:41 -0700

    signup function in accounts/forms.py and  accounts/views.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,62 @@
 from django import forms
 from .models import Account, UserProfile
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
 
 class SignupForm(forms.ModelForm):
-    pass
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name',
+                  'phone_number', 'email', 'password')
+
+    def clean(self):
+        cleaned_data = super(SignupForm, self).clean()
+        password = self.cleaned_data['password']
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+
+    def save(self, request):
+
+        user = super(forms.ModelForm, self).save(commit=False)
+        user.is_active = False
+        user.save()
+
+        context = {
+            # 'from_email': settings.DEFAULT_FROM_EMAIL,
+            'request': request,
+            'protocol': request.scheme,
+            'username': self.cleaned_data.get('username'),
+            'domain': request.META['HTTP_HOST'],
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': default_token_generator.make_token(user),
+        }
+
+        subject = render_to_string(
+            'djangobin/email/activation_subject.txt', context)
+        email = render_to_string(
+            'djangobin/email/activation_email.txt', context)
+
+        send_mail(subject, email, settings.DEFAULT_FROM_EMAIL, [user.email])
+
+        return user
 
 
 class UserForm(forms.ModelForm):
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignupForm, UserForm, UserProfileForm
 from django.http import HttpResponse
 
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 # Create your views here.
 
 
@@ -27,15 +35,42 @@ def signup(request):
         # that includes the data from the browser
         form = SignupForm(request.POST)
         if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = form.cleaned_data['username']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password,
+            )
+            user.phone_number = phone_number
             # This will add the user to the database
-            user = form.save()
+            user.save()
+
+            # create a new user profile
+            profile = UserProfile()
+            profile_user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
+            # activate the user
+            current_site = get_current_site(request)
+            mail_subject = "Hello beautiful, activate your account"
+            msg = render_to_string('accounts/verify_email.html')
+
             # This is how we log a user in via code
             login(request, user)
             return redirect('index')
         else:
             error_message = 'Invalid sign up - try again'
     # A bad POST or a GET request, so render signup.html with an empty form
-    form = UserCreationForm()
+    form = SignupForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'accounts/signup.html', context)
 
