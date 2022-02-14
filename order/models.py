e99dd308befce0636c5390cc4263fdbece377fd3
CodyMurray-ga <codakmurraki@gmail.com>
2022-02-14 17:44:36 -0600

    migrate

--- order/models.py
+++ order/models.py
@@ -1,28 +1,19 @@
-import email
-from email.policy import default
 from django.db import models
-from django.forms import BooleanField, CharField, DateTimeField, EmailField, FloatField
 
 # Create your models here.
 class Order(models.Model):
     user = models.ForeignKey()
-    order_number = CharField(max_length=20)
-    first_name = CharField(max_length=20)
-    last_name = CharField(max_length=20)
-    phone = CharField(max_length=11)
-    email = EmailField()
-    address_line_1 = CharField(max_length=50)
-    address_line_2 = CharField(max_length=50)
-    country = CharField(max_length=30)
-    state = CharField(max_length=30)
-    city = CharField(max_length=30)
-    order_note = CharField(max_length=300)
-    order_total = FloatField()
-    tax = FloatField()
-    status = CharField(max_length=30)
-    is_ordered = BooleanField(default=True)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now_add=True)
+    order_note = models.CharField(max_length=300)
+    order_total = models.FloatField()
+    status = models.CharField(max_length=30)
+    is_ordered = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.order_number
+
+class Cart(models.Model):
+    product = models.ForeignKey(on_delete=models.CASCADE)
+
