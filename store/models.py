a64448d91eda24bf3d627ec9bc455352318586bf
taniryla <ryland0000@gmail.com>
2022-02-15 14:34:51 -0700

    float field

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.FloatField()
     image = models.ImageField(blank=True, upload_to='images/')
     stock = models.PositiveIntegerField()
 
