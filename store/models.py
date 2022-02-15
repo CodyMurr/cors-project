5c9546bca850b8a281bcb4e7b94ea41ec2d3a285
CodyMurray-ga <codakmurraki@gmail.com>
2022-02-15 14:29:37 -0600

    fix error in db

--- store/models.py
+++ store/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.urls import reverse
-
+from accounts.models import Account
 # Create your models here.
 
+RATING=(
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),    
+)
+
+class Review(models.Model):
+    account=models.ForeignKey(Account,on_delete=models.CASCADE)
+    rating: models.CharField(choices=RATING, max_length=10)
+    subject: models.CharField(max_length=100)
+    content: models.TextField(max_length=300)
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -12,12 +25,14 @@ class Product(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     stock = models.PositiveIntegerField()
 
+
     # required
 
     is_available = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now_add=True)
+    review=models.ForeignKey(Review, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name_plural = 'Products'
