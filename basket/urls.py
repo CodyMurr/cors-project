407407dda71fadb7b594fb19813ac0264ff28620
mehjwazir <mehjwazir@gmail.com>
2022-02-17 11:08:15 -0800

    updated basket views urls

--- basket/urls.py
+++ basket/urls.py
@@ -6,11 +6,5 @@ app_name = 'basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
     path('add/<int:id>/', views.basket_add, name='basket_add'),
-    path('item_clear/<int:id>/', views.item_clear, name='item_clear'),
-    path('item_increment/<int:id>/',
-         views.item_increment, name='item_increment'),
-    path('item_decrement/<int:id>/',
-         views.item_decrement, name='item_decrement'),
-    path('basket_clear/', views.basket_clear, name='basket_clear'),
-    path('basket_detail/', views.basket_detail, name='basket_detail'),
+   
 ]
