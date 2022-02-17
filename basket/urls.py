f8c14a8eb20bb0aaf8a8cee60e766cc9a0729962
mehjwazir <mehjwazir@gmail.com>
2022-02-17 11:32:40 -0800

    updated basket urls delete function

--- basket/urls.py
+++ basket/urls.py
@@ -6,5 +6,5 @@ app_name = 'basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
     path('add/<int:id>/', views.basket_add, name='basket_add'),
-   
+    path('delete/<int:id>/', views.basket_delete, name='basket_delete'),
 ]
