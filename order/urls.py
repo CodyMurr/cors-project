878c179549dbb59090dde587e291989f2f2d80ce
skylor727 <skylor1997@hotmail.com>
2022-02-17 18:52:31 -0700

    fix store button and add ability to view past orders

--- order/urls.py
+++ order/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('<int:order_id>/', views.order_detail, name='order_detail'),
     path('checkout/', views.checkout, name='checkout'),
+    path('cart/', views.find_cart, name='find_cart'),
+    path('orders/', views.get_orders, name='get_orders'),
 ]
